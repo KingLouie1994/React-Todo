9e160d8881a82b8d5cadaae93bc7c6dfd6f10af7
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2019-12-02 15:23:40 +0100

    added all states to app.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,60 @@
-import React from 'react';
-import TodoForm from './components/TodoComponents/TodoForm';
-import TodoList from './components/TodoComponents/TodoList';
+import React from "react";
+import TodoList from "./components/TodoComponents/TodoList";
+import TodoForm from "./components/TodoComponents/TodoForm";
+import "./App.css";
 
-class App extends React.Component {
+export default class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [],
+      formValue: ""
+    };
+  }
+
+  setTodos = value => {
+    this.setState(currentState => ({
+      todos: currentState.todos.concat({ name: value, isCompleted: false })
+    }));
+  };
+
+  markComplete = arr => {
+    this.setState(currentState => ({
+      todos: arr
+    }));
+  };
+
+  setFormValue = value => {
+    this.setState(currentState => ({
+      formValue: value
+    }));
+  };
+
+  clearCompleted = arr => {
+    this.setState(currentState => ({
+      todos: arr
+    }));
+  };
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
-        <TodoForm />
-        <TodoList />
+        <h2>Todo List</h2>
+        <TodoList 
+          todos={this.state.todos} 
+          markComplete={this.markComplete} />
+        <TodoForm
+          formValue={this.state.formValue}
+          todos={this.state.todos}
+          setTodos={this.setTodos}
+          setFormValue={this.setFormValue}
+          clearCompleted={this.clearCompleted}
+        />
       </div>
     );
   }
 }
-
-export default App;
--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class TodoForm extends React.Component {
     render() {
         return(
-            <form>
+            <form onSubmit={this.props.addItem}>
                 <input type='text' placeholder='new task' />
                 <button>Add Todo</button>
                 <button>Clear everything</button>
