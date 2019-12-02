6012a477d16576512946d064acb3a8b0cbb4ddf2
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2019-12-02 14:53:37 +0100

    Built first draft of TodoList and imported it to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TodoForm from './components/TodoComponents/TodoForm';
+import TodoList from './components/TodoComponents/TodoList';
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
@@ -10,6 +11,7 @@ class App extends React.Component {
       <div>
         <h2>Welcome to your Todo App!</h2>
         <TodoForm />
+        <TodoList />
       </div>
     );
   }
